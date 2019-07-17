f93768b131b48e612f6ab402a3206d49be2def15
Md Shoaib <shoaib05oct@gmail.com>
2019-07-17 22:06:17 +0530

    Version 1

---
+++ biggest of three.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+	//declaration of values
+	int value1, value2, value3, big = 0;
+	
+	//entering the values
+	printf("Type your first value:\n");
+	scanf("%d", & value1);
+	
+	printf("Type your second value:\n");
+	scanf("%d", & value2);
+	
+	printf("Type your third value:\n");
+	scanf("%d", & value3);
+	
+	//logic usinf if condition
+	
+	if(value1 > big)
+	{
+		big = value1;
+	}
+	
+	if(value2 > big)
+	{
+		big = value2;
+	}
+	
+	if(value3 > big)
+	{
+		big = value3;
+	}
+	
+	//output
+	printf("The biggest value is:%d\n", big);
+	
+	getchar();
+	return 0;
+}
+
